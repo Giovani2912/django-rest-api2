5afb81476f015a0ec4281f1eef55f6c2464b0382
Giovani Foltran <giovanicanovaf@gmail.com>
2021-01-12 14:48:33 -0300

    fim

--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-
+    
+    # Ordenação dos campos no backend (api)
+    # Chamando as requisiçoes, por exemplo --> Ordenar, Buscar
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    # Declarando o campo para a ordenação
+    ordering_fields = ['nome']
+    # Declarando o campo para a busca
+    search_fields = ['nome', 'cpf']
+    # Declarando a busca por ativos ou não
+    filterset_fields = ['ativo']
